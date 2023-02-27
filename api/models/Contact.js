2f6b932e9aa14631bd6422575a3156ff8e02dbd7
Joyesh Wadhwa <65168230+jontyxo@users.noreply.github.com>
2023-02-27 19:43:38 +0530

    Update Contact.js

--- api/models/Contact.js
+++ api/models/Contact.js
@@ -4,8 +4,7 @@ const ContactSchema = new mongoose.Schema(
   {
     email: {
       type: String,
-      required: true,
-      unique: true,
+      required: true
     },
     message:{
         type:String,
